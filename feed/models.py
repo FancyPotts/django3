010f5bdce5f5e68eeffa32f7071ca372d248fb59
pottsaf <aaronfpotts@gmail.com>
2023-03-03 18:27:09 -0700

    Added author element to post model

--- feed/models.py
+++ feed/models.py
@@ -1,11 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
 
 
 
 class Post(models.Model):
     text = models.CharField(max_length=240)
     date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        
+    )
     
     def __str__(self):
         return self.text[0:100]
